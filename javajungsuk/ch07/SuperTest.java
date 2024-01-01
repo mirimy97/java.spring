090c859ea3d22bc7ecf8aa35c51fd222b8a0ca06
mirimy <mirimy97@gmail.com>
2024-01-01 23:02:07 +0900

    this, super 참조변수 설명

--- javajungsuk/ch07/SuperTest.java
+++ javajungsuk/ch07/SuperTest.java
@@ -12,7 +12,13 @@ class Parent {
 }
 
 class Child extends Parent {
+    int x=20;
     void method() {
+        /**
+         * - 같은 이름의 멤버 변수를 조상 / 자손 클래스에 중복 정의가 가능하며, 아래와 같이 구분한다.
+         * 자손 클래스에 선언된 멤버 변수 : x / this.x
+         * 상속받은 멤버 변수 : super.x
+         */
         System.out.println("x=" + x);
         System.out.println("this.x=" + this.x);
         System.out.println("super.x=" + super.x);
