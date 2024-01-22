4dd7eaa50ada00abdefb4f2da0e22508f5e2c86a
mirimy <mirimy97@gmail.com>
2024-01-22 18:06:02 +0900

    AppConfig 리팩터링 - 역할 분리

--- springcore-basic/core/src/main/groovy/hello/core/order/AppConfig.java
+++ springcore-basic/core/src/main/groovy/hello/core/order/AppConfig.java
@@ -1,19 +1,32 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
+/**
+ * 역할이 잘 드러나게 리팩터링
+ *  역할 분리 (repository 생성 / discountPolicy 생성)
+ */
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         // 여기만 FixDiscountPolicy <-> RateDiscountPolicy로 할인 정책 변경하면 다른 코드 변경 없이 사용할 수 있다.
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
