6071710c57a2c1869ebb7e87cf8c0add73e3520e
mirimy <mirimy97@gmail.com>
2024-01-23 00:41:24 +0900

    스프링 빈 조회 - 기본

---
+++ springcore-basic/core/src/test/groovy/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,54 @@
+package hello.core.beanfind;
+
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * ( 빈 이름 조회 방법 )
+ *  - ac.getBean(이름, 타입)
+ *  - ac.getBean(타입)
+ *  - 조회 대상 빈이 없으면 에러 발생
+ */
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+//        System.out.println("memberService = " + memberService);
+//        System.out.println("memberService.getClass() = " + memberService.getClass());
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX() {
+//        MemberService xxxx = ac.getBean("xxxx", MemberService.class); => 해당하는 빈 없음 에러 발생
+        // Assertions(Junit).assertThrows : 예외가 발생해야 Test가 성공
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxx", MemberService.class));
+    }
+}
