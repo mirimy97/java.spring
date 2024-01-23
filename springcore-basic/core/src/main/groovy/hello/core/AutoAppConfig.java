c99bca70bb020f22a016eb58ada5d33931c56214
mirimy <mirimy97@gmail.com>
2024-01-23 17:26:18 +0900

    컴포넌트 스캔 탐색 범위 지정

--- springcore-basic/core/src/main/groovy/hello/core/AutoAppConfig.java
+++ springcore-basic/core/src/main/groovy/hello/core/AutoAppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.FilterType;
  */
 @Configuration   // 설정 정보 class
 @ComponentScan(
+        basePackages = "hello.core",   // 탐색을 시작할 패키지를 지정할 수 있음. 그밖의 패키지는 탐색하지 않는다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )    // 컴포넌트 스캔 , Filter : 기존 수동으로 설정한 AppConfig class(@Configuration 붙음)를 제외하기 위해 설정해주었다.
 public class AutoAppConfig {
