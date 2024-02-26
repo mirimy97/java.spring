a7d3c9da63283dc3634b407ee8c6296c8969314c
mirimy <mirimy97@gmail.com>
2024-02-27 01:46:03 +0900

    java jpa 프로젝트

---
+++ spring-mvc1/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,8 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+}
